d7572e61a4e8f6a07d493b36d1bedd976a51125a
Talon <olivetto1989@mail.com>
2023-03-16 17:00:15 +0100

    .

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('working')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
