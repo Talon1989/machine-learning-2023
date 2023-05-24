373117e63ef070021757bcc26918e48cba07b542
Talon <olivetto1989@mail.com>
2023-05-24 09:17:10 +0200

    .

--- reinforcement_learning/DiscreteEnvAgent.py
+++ reinforcement_learning/DiscreteEnvAgent.py
@@ -1,9 +1,12 @@
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow_probability
+
 from Utilities import ReplayBuffer
 from CustomUtilities import print_graph
 import tensorflow as tf
+import tensorflow_probability as tfp
 keras = tf.keras
 
 
@@ -76,8 +79,8 @@ class DeepQLearningTensorflow:
             y_hat = np.copy(y_pred)
             y_hat[np.arange(y_pred.shape[0]), actions] = markov_pred
             y_hat = tf.convert_to_tensor(y_hat, dtype=tf.float32)
-            # loss = self.loss_function(y_pred, y_hat)
-            loss = tf.reduce_mean((y_hat - y_pred) ** 2)
+            loss = self.loss_function(y_pred, y_hat)
+            # loss = tf.reduce_mean((y_hat - y_pred) ** 2)
         grads = tape.gradient(loss, self.main_nn.trainable_variables)
         self.main_nn.optimizer.apply_gradients(zip(grads, self.main_nn.trainable_variables))
         if self.epsilon > 1/100:
@@ -107,6 +110,7 @@ class DeepQLearningTensorflow:
                 print('Environment %s solved at episode %d' % (self.env.unwrapped.spec.id, ep))
                 if save_model:
                     self.main_nn.save(filepath='../data/dql_nn_1/main_nn')
+                    print('model saved.')
                 return self
             scores.append(score)
             avg_scores.append(np.sum(scores[-50:]) / len(scores[-50:]))
@@ -130,6 +134,7 @@ class DeepQLearningTensorflow:
 
     def test_model(self, n_episodes=5):
         self.epsilon = 0
+        self.load_model()
         for ep in range(1, n_episodes):
             score = 0
             s = self.env.reset()[0]
@@ -143,9 +148,129 @@ class DeepQLearningTensorflow:
             print('Episode %d | Score: %.3f' % (ep, score))
 
 
-agent = DeepQLearningTensorflow(env_, [16, 16, 32, 32])
+class PolicyGradientMethodTensorflow:
+
+    def __init__(
+            self,
+            env: gym.wrappers.time_limit.TimeLimit,
+            hidden_shape,
+            alpha=1/1_000,
+            gamma=99/100
+    ):
+        self.env = env
+        self.n_s, self.n_a = env.observation_space.shape[0], env.action_space.n
+        self.gamma = gamma
+        self.buffer = ReplayBuffer(max_size=5_000)
+        self.nn = self._build_nn(hidden_shape)
+        self.nn.compile(optimizer=keras.optimizers.Adam(learning_rate=alpha))
+
+    def _build_nn(self, hidden_shape):
+        def custom_softmax(x):
+            return tf.exp(x) / tf.reshape(tf.reduce_sum(tf.exp(x), axis=1), [-1, 1])
+        input_layer = keras.layers.Input(shape=self.n_s)
+        layer = input_layer
+        for h in hidden_shape:
+            layer = keras.layers.Dense(units=h, activation='relu')(layer)
+        output_layer = keras.layers.Dense(units=self.n_a, activation='softmax')(layer)
+        model = keras.models.Model(input_layer, output_layer)
+        return model
+
+    def _custom_policy_gradient_loss(self, states, actions, norm_returns):
+        probabilities = self.nn(states)
+        distribution = tfp.distributions.Categorical(probs=probabilities, dtype=tf.float32)
+        log_probability_of_actions = distribution.log_prob(value=actions)
+        loss = - (log_probability_of_actions * norm_returns)
+        return loss
+
+    #  hardcoded to work with a single state
+    def _choose_action(self, s):
+        s = tf.convert_to_tensor([s])
+        distribution = tfp.distributions.Categorical(probs=self.nn(s), dtype=tf.float32)
+        return int(distribution.sample().numpy())
+
+    def _store_transition(self, s, a, r):
+        self.buffer.remember(s, a, r, None, None)
+
+    def load_model(self, filepath='../data/pgm_nn_1/nn'):
+        self.nn = keras.models.load_model(filepath)
+
+    def _train(self):
+        states, actions, rewards, _, _ = self.buffer.get_buffer(
+            batch_size=self.buffer.get_buffer_size(),
+            randomized=False,
+            cleared=True
+        )
+        returns = []
+        discounted_sum = 0
+        for r in reversed(rewards):
+            discounted_sum = r + self.gamma * discounted_sum
+            returns.append(discounted_sum)
+        returns = returns[::-1]
+        normalized_returns = (returns - np.mean(returns)) / (np.std(returns) + 1e-16)
+        with tf.GradientTape() as tape:
+            # probabilities = self.nn(states)
+            # distribution = tfp.distributions.Categorical(probs=probabilities, dtype=tf.float32)
+            # log_probability_of_actions = distribution.log_prob(value=actions)
+            # loss = - (log_probability_of_actions * normalized_returns)
+            loss = self._custom_policy_gradient_loss(states, actions, normalized_returns)
+        grads = tape.gradient(loss, self.nn.trainable_variables)
+        self.nn.optimizer.apply_gradients(zip(grads, self.nn.trainable_variables))
+
+    def fit(self, n_episodes=5_000, graph=True, save_model=False):
+        scores, avg_scores = [], []
+        counter = 0
+        for ep in range(1, n_episodes+1):
+            s = self.env.reset()[0]
+            score = 0
+            for i in range(self.env._max_episode_steps):
+                a = self._choose_action(s)
+                s_, r, d, t, _ = self.env.step(a)
+                score += r
+                # r = 0 if d == 0 else -100
+                self._store_transition(s, a, r)
+                if d or t:
+                    if i >= self.env._max_episode_steps - 1:
+                        counter += 1
+                    else:
+                        counter = 0
+                    break
+                s = s_
+            self._train()
+            scores.append(score)
+            avg_scores.append(np.sum(scores[-50:]) / len(scores[-50:]))
+            if counter >= 5:
+                print('Environment %s solved at episode %d' % (self.env.unwrapped.spec.id, ep))
+                if save_model:
+                    self.nn.save(filepath='../data/pgm_nn_1/main_nn')
+                    print('model saved.')
+            if ep % 10 == 0:
+                print('Episode %d | avg score: %.3f' % (ep, avg_scores[-1]))
+            if ep % 200 == 0 and graph:
+                print_graph(scores, avg_scores, 'scores', 'avg scores', 'PGM episode %d' % ep)
+        return self
+
+    def test_model(self, n_episodes=5):
+        self.load_model()
+        for ep in range(1, n_episodes):
+            score = 0
+            s = self.env.reset()
+            while True:
+                a = self._choose_action(s)
+                s_, r, d, t, _ = self.env.step(a)
+                score += r
+                if d or t:
+                    break
+                s = s_
+            print('Episode %d | score: %.3f' % (ep, score))
+        return self
+
+
+agent = PolicyGradientMethodTensorflow(env_, [16, 16, 32, 32])
 agent.fit()
 
+# agent = DeepQLearningTensorflow(env_, [16, 16, 32, 32])
+# agent.fit(graph=False, save_model=False)
+# agent.test_model()
 
 
 
