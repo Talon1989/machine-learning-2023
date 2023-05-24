8e0a9be2e954fe5b790cf490d37463e202a22c58
Talon <olivetto1989@mail.com>
2023-05-24 16:34:54 +0200

    .

--- reinforcement_learning/DiscreteEnvAgent.py
+++ reinforcement_learning/DiscreteEnvAgent.py
@@ -7,8 +7,8 @@ from Utilities import ReplayBuffer
 from CustomUtilities import print_graph
 import tensorflow as tf
 import tensorflow_probability as tfp
-keras = tf.keras
 
+keras = tf.keras
 
 np.random.seed(42)
 env_ = gym.make('CartPole-v1')
@@ -20,10 +20,10 @@ class DeepQLearningTensorflow:
             self,
             env: gym.wrappers.time_limit.TimeLimit,
             hidden_shape,
-            alpha=1/1_000,
-            gamma=99/100,
-            epsilon_decay=9_999/10_000,
-            batch_size=2**6
+            alpha=1 / 1_000,
+            gamma=99 / 100,
+            epsilon_decay=9_999 / 10_000,
+            batch_size=2 ** 6
     ):
         self.env = env
         self.n_s, self.n_a = self.env.observation_space.shape[0], self.env.action_space.n
@@ -83,13 +83,13 @@ class DeepQLearningTensorflow:
             # loss = tf.reduce_mean((y_hat - y_pred) ** 2)
         grads = tape.gradient(loss, self.main_nn.trainable_variables)
         self.main_nn.optimizer.apply_gradients(zip(grads, self.main_nn.trainable_variables))
-        if self.epsilon > 1/100:
+        if self.epsilon > 1 / 100:
             self.epsilon = self.epsilon * self.epsilon_decay
 
     def fit(self, n_episodes=5_000, graph=True, save_model=False):
         scores, avg_scores = [], []
         counter = 0
-        for ep in range(1, n_episodes+1):
+        for ep in range(1, n_episodes + 1):
             score = 0
             s = self.env.reset()[0]
             for i in range(self.env._max_episode_steps):
@@ -154,8 +154,8 @@ class PolicyGradientMethodTensorflow:
             self,
             env: gym.wrappers.time_limit.TimeLimit,
             hidden_shape,
-            alpha=1/1_000,
-            gamma=99/100
+            alpha=1 / 1_000,
+            gamma=99 / 100
     ):
         self.env = env
         self.n_s, self.n_a = env.observation_space.shape[0], env.action_space.n
@@ -167,6 +167,7 @@ class PolicyGradientMethodTensorflow:
     def _build_nn(self, hidden_shape):
         def custom_softmax(x):
             return tf.exp(x) / tf.reshape(tf.reduce_sum(tf.exp(x), axis=1), [-1, 1])
+
         input_layer = keras.layers.Input(shape=self.n_s)
         layer = input_layer
         for h in hidden_shape:
@@ -219,7 +220,7 @@ class PolicyGradientMethodTensorflow:
     def fit(self, n_episodes=5_000, graph=True, save_model=False):
         scores, avg_scores = [], []
         counter = 0
-        for ep in range(1, n_episodes+1):
+        for ep in range(1, n_episodes + 1):
             s = self.env.reset()[0]
             score = 0
             for i in range(self.env._max_episode_steps):
@@ -265,28 +266,133 @@ class PolicyGradientMethodTensorflow:
         return self
 
 
-agent = PolicyGradientMethodTensorflow(env_, [16, 16, 32, 32])
-agent.fit()
+class PolicyGradientActorCriticTensorflow:
+
+    def __init__(
+            self,
+            env: gym.wrappers.time_limit.TimeLimit,
+            hidden_shape_1,
+            hidden_shape_2,
+            alpha=1 / 1_000,
+            beta=1 / 500,
+            gamma=99 / 100
+    ):
+        self.env = env
+        self.n_s, self.n_a = env.observation_space.shape[0], env.action_space.n
+        self.gamma = gamma
+        self.buffer = ReplayBuffer(max_size=5_000)
+        self.actor_nn = self._build_actor(hidden_shape_1)
+        self.critic_nn = self._build_critic(hidden_shape_2)
+        self.actor_nn.compile(optimizer=keras.optimizers.Adam(learning_rate=alpha))
+        self.critic_nn.compile(optimizer=keras.optimizers.Adam(learning_rate=beta))
+        self.critic_loss_function = keras.losses.MeanSquaredError()
+
+    def _build_actor(self, hidden_shape):
+        input_layer = keras.layers.Input(shape=self.n_s)
+        layer = input_layer
+        for h in hidden_shape:
+            layer = keras.layers.Dense(units=h, activation='relu')(layer)
+        output_layer = keras.layers.Dense(units=self.n_a, activation='softmax')(layer)
+        model = keras.models.Model(input_layer, output_layer)
+        return model
+
+    #  outputting state value function
+    def _build_critic(self, hidden_shape):
+        input_layer = keras.layers.Input(shape=self.n_s)
+        layer = input_layer
+        for h in hidden_shape:
+            layer = keras.layers.Dense(units=h, activation='relu')(layer)
+        output_layer = keras.layers.Dense(units=1, activation='linear')(layer)
+        model = keras.models.Model(input_layer, output_layer)
+        return model
+
+    def _choose_action(self, s):
+        s = tf.convert_to_tensor([s])
+        distribution = tfp.distributions.Categorical(probs=self.actor_nn(s), dtype=tf.float32)
+        return int(distribution.sample().numpy())
+
+    def _store_transition(self, s, a, r):
+        self.buffer.remember(s, a, r, None, None)
+
+    def _load_model(self, filepath='../data/actor_critic_nn_1/actor_nn'):
+        self.actor_nn = keras.models.load_model(filepath)
+
+    def _train(self):
+
+        states, actions, rewards, _, _ = self.buffer.get_buffer(
+            batch_size=self.buffer.get_buffer_size(),
+            randomized=False,
+            cleared=True
+        )
+        returns = []
+        discounted_sum = 0
+        for r in reversed(rewards):
+            discounted_sum = r + self.gamma * discounted_sum
+            returns.append(discounted_sum)
+        returns = returns[::-1]
+
+        with tf.GradientTape() as actor_tape:
+
+            probabilities = self.actor_nn(states)
+            distribution = tfp.distributions.Categorical(probs=probabilities, dtype=tf.float32)
+            actions_log_probability = distribution.log_prob(actions)
+
+            state_values = self.critic_nn(states)
+            advantages = np.reshape(returns, [-1, 1]) - state_values
+
+            actor_loss = - tf.reduce_sum(actions_log_probability * advantages)
+
+        actor_grads = actor_tape.gradient(actor_loss, self.actor_nn.trainable_variables)
+        self.actor_nn.optimizer.apply_gradients(zip(actor_grads, self.actor_nn.trainable_variables))
+
+        with tf.GradientTape() as critic_tape:
+
+            state_values = self.critic_nn(states)
+            critic_loss = self.critic_loss_function(returns, state_values)
+
+        critic_grads = critic_tape.gradient(critic_loss, self.critic_nn.trainable_variables)
+        self.critic_nn.optimizer.apply_gradients(zip(critic_grads, self.critic_nn.trainable_variables))
+
+    def fit(self, n_episodes=5_000, graph=True, save_model=False):
+        scores, avg_scores = [], []
+        counter = 0
+        for ep in range(1, n_episodes + 1):
+            s = self.env.reset()[0]
+            score = 0
+            for i in range(self.env._max_episode_steps):
+                a = self._choose_action(s)
+                s_, r, d, t, _ = self.env.step(a)
+                score += r
+                # r = 0 if d == 0 else -100
+                self._store_transition(s, a, r)
+                if d or t:
+                    if i >= self.env._max_episode_steps - 1:
+                        counter += 1
+                    else:
+                        counter = 0
+                    break
+                s = s_
+            self._train()
+            scores.append(score)
+            avg_scores.append(np.sum(scores[-50:]) / len(scores[-50:]))
+            if counter >= 5:
+                print('Environment %s solved at episode %d' % (self.env.unwrapped.spec.id, ep))
+                if save_model:
+                    self.actor_nn.save(filepath='../data/actor_critic_nn_1/actor_nn')
+                    print('model saved.')
+            if ep % 10 == 0:
+                print('Episode %d | avg score: %.3f' % (ep, avg_scores[-1]))
+            if ep % 200 == 0 and graph:
+                print_graph(scores, avg_scores, 'scores', 'avg scores', 'PGM episode %d' % ep)
+        return self
+
+
+agent = PolicyGradientActorCriticTensorflow(env_, [16, 16, 32, 32], [16, 32, 64])
+agent.fit(graph=False)
+
+# agent = PolicyGradientMethodTensorflow(env_, [16, 16, 32, 32])
+# agent.fit()
 
 # agent = DeepQLearningTensorflow(env_, [16, 16, 32, 32])
 # agent.fit(graph=False, save_model=False)
 # agent.test_model()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
